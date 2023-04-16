16df3b9fb8296cfa49df9c4ec3c3d8d9aa1e62e0
Kireobat <erik.flatabo@gmail.com>
2023-04-16 21:28:38 +0200

    frontend example

---
+++ src/lib/components/Example.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { example } from "$lib/stores.js";
+
+  let word = "";
+  example.subscribe((value) => {
+    word = value;
+  });
+</script>
+
+<h1>This is an {word}</h1>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Example from "$lib/components/Example.svelte";
+  import "../app.css";
+</script>
+
+<main>
+  <h1>This is a preset</h1>
+</main>
+
+<style lang="postcss">
+</style>
